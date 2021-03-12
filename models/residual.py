a77f9988645ef0dd9d8a8e6092118b95ded5262a
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-03-12 14:05:28 -0500

    debug resblock

--- models/residual.py
+++ models/residual.py
@@ -56,6 +56,7 @@ if __name__ == "__main__":
     x = np.random.random_sample((5, 39, 512))
     x = torch.tensor(x).float()
     # test Residual Layer
+    print("Input size", x.shape)
     res = ResidualLayer(39, 768, 768)
     res_out = res(x)
     #print('Res Layer out shape:', res_out.shape)
